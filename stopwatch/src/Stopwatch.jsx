0727c50166ff1e11c0e787a4178777a2cd62f95d
[Aakarsh Sharma] <[aakarshjai@gmail.com]>
2024-10-19 04:30:31 +0530

    changes

--- stopwatch/src/Stopwatch.jsx
+++ stopwatch/src/Stopwatch.jsx
@@ -1,10 +1,63 @@
-
+import React, {useState, useEffect, useRef} from "react";
 
 function Stopwatch(){
 
+    const [isRunnning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() =>{
+        if (isRunnning){
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10);
+        }
+
+        return () =>{
+            clearInterval(intervalIdRef.current);
+        }
+
+
+    }, [isRunnning]);
+
+    function start(){
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+    }
+
+    function stop(){
+        setIsRunning(false);
+    }
+
+    function reset(){
+        setElapsedTime(0);
+        setIsRunning(false);
+    }
+
+    function formatTime(){
+        let hours = Math.floor(elapsedTime / (1000*60*60));
+        let minutes = Math.floor(elapsedTime / (100*60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let miliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        miliseconds = String(miliseconds).padStart(2, "0");
+
+        return `${hours}:${minutes}:${seconds}:${miliseconds}`
+    }
+
+
     return(
-        <div>
-            <h1>Stopwatch</h1>
+        <div className="stopwatch">
+            <div className="display">{formatTime()}</div>
+            <div className="controls">
+                <button className="start-button" onClick={start}>start</button>
+                <button className="stop-button" onClick={stop}>stop</button>
+                <button className="reset-button" onClick={reset}>reset</button>
+            </div>
         </div>
     );
 }
